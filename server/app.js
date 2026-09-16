6921f6d10c74ab0600734ef7cba7e0d36fd8aedb
MlungisiMahlangu <mahlangumlungisi992@gmail.com>
2026-09-17 00:48:41 +0200

    Add auth module: register, login, JWT middleware

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import { db } from './config/firebase.js';
+import authRoutes from './modules/auth/auth.route.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'E-SafetyRides API is running' });
+    res.json({ message: 'E-SafetyRides API is running' });
 });
 
 
